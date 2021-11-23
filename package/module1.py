52013eaee3753835fe21146e1a1db59bb36d7988
AndreaOrlando23 <64850221+AndreaOrlando23@users.noreply.github.com>
2021-11-23 20:03:46 +0100

    refactor: add command guide to test module1 function

--- package/module1.py
+++ package/module1.py
@@ -5,7 +5,6 @@ class Module1:
 
     def __str__(self):
         return "Module 1 - It works!"
-    
 
     def response_from_module2(self):
         response = Module2()
@@ -20,5 +19,10 @@ def main():
     test_module2 = test_module1.response_from_module2()
     print(test_module2)  # output: Module2 - It Works
 
+
 if __name__ == '__main__':
     main()
+
+
+# use the following command positioned in the root (import-test) to test it out
+# >> python3 -m package.module1
